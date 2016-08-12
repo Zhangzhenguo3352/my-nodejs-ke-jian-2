59c1f189e699bf550a5de4debb6dc5bf91554e31
“Zhangzhenguo3352git config --global user.email “13012113210@163.com <13012113210@163.comgit config --global user.email 13012113210@163.com>
2016-08-12 08:13:01 +0800

    nodejs 课件7-31

--- 2-appedFile.js
+++ 2-appedFile.js
@@ -1,5 +1,5 @@
 // 文件相关操作
-//   创建文件    fs.appendFile('找加内容的文件及路径'，‘找加的内容’，fcDb(err))
+//  找加内容     fs.appendFile('找加内容的文件及路径'，‘找加的内容’，fcDb(err))
 var fs = require('fs');
 
 fs.appendFile('a.txt','\n1231231',function(err){
